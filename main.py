d0ef2a2c4222b1454d145c8d47ee8ad967918147
Jan Zalesiński <janzalesinski@gmail.com>
2024-02-09 19:09:08 +0100

    added registration (not finished yet), add new button to show new frame

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import wx
 import requests
 import time
 
+
 class MainPanel(wx.Panel):
     def __init__(self, parent):
         super(MainPanel, self).__init__(parent)
@@ -13,26 +14,33 @@ class MainPanel(wx.Panel):
         sizer = wx.BoxSizer(wx.VERTICAL)
         login_label = wx.StaticText(self, label="Podaj email")
         sizer.Add(login_label, 0, wx.ALL | wx.CENTER, 5)
-        self.login_field = wx.TextCtrl(self,style=wx.TE_PROCESS_ENTER)
-        sizer.Add(self.login_field,0,wx.ALL | wx.CENTER,5)
+        self.login_field = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+        sizer.Add(self.login_field, 0, wx.ALL | wx.CENTER, 5)
         self.password_label = wx.StaticText(self, label="Podaj hasło")
         sizer.Add(self.password_label, 0, wx.ALL | wx.CENTER, 10)
-        self.password_field =wx.TextCtrl(self, style=wx.TE_PASSWORD)
-        sizer.Add(self.password_field,0,wx.ALL | wx.CENTER,5)
+        self.password_field = wx.TextCtrl(self, style=wx.TE_PASSWORD)
+        sizer.Add(self.password_field, 0, wx.ALL | wx.CENTER, 5)
         button = wx.Button(self, label="Zaloguj się")
+        register_button = wx.Button(self, label="Nie masz konta? Zarejestruj się!")
         button.Bind(wx.EVT_BUTTON, self.onLogin)
+        register_button.Bind(wx.EVT_BUTTON, self.onRegister)
         sizer.Add(button, 0, wx.ALL | wx.CENTER, 5)
+        sizer.Add(register_button, 0, wx.ALL | wx.CENTER, 5)
         self.SetSizer(sizer)
+
+    def onRegister(self, event):
+        self.parent.showRegisterPanel()
+        pass
 
     def onLogin(self, event):
         login = self.login_field.GetValue()
         password = self.password_field.GetValue()
-        response = self.parent.listPanel.checkLogin(login,password)
+        response = self.parent.listPanel.checkLogin(login, password)
         if response.status_code == 200:
             print(response)
             self.parent.showListPanel()
-            
-            
+
+
         else:
             print(f"Błąd: {response.status_code}")
             print(response)
@@ -43,43 +51,109 @@ class MainPanel(wx.Panel):
 
 class ListPanel(wx.Panel):
     def __init__(self, parent):
-        super(ListPanel, self).__init__(parent,size=(400,700))
+        super(ListPanel, self).__init__(parent, size=(400, 700))
         sizer = wx.BoxSizer(wx.HORIZONTAL)
-        
-        # Dodaj widżety do głównego panelu
         label = wx.StaticText(self, label="Treść głównego panelu")
         sizer.Add(label, 0, wx.ALL | wx.CENTER, 5)
         bottom_panel = wx.Panel(self)
+        add_button = wx.Button(bottom_panel, label="Dodaj")
+        add_button.Bind(wx.EVT_BUTTON, self.show_add_form)
 
-        # Dodawanie przycisków do panelu
-        start_button = wx.Button(bottom_panel, label="Start")
         settings_button = wx.Button(bottom_panel, label="Ustawienia")
+        logout_button = wx.Button(bottom_panel, label="Wyloguj")
 
-        # Ustawienie layoutu dla przycisków
         sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(start_button, 0, wx.ALL, 10)
+        sizer.Add(add_button, 0, wx.ALL, 10)
         sizer.Add(settings_button, 0, wx.ALL, 10)
+        sizer.Add(logout_button, 0, wx.ALL, 10)
         bottom_panel.SetSizer(sizer)
-
-        # Dodanie panelu do głównego okna
         main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.AddStretchSpacer()  # Dodaj elastyczny odstęp na górze
+        main_sizer.AddStretchSpacer()
         main_sizer.Add(bottom_panel, 0, wx.EXPAND)
         self.SetSizer(main_sizer)
-        
+    def show_add_form(self,event):
+        new_form = AddForm()
+        new_form.Show()
 
-
-    def checkLogin(self, login,password):
+    def checkLogin(self, login, password):
         busy_info = wx.BusyInfo("Trwa logowanie...")
         url = "https://finanse.xce.pl/login"
         data = {
             'email': login,
             'password': password
         }
-        
-        response = requests.post(url,json=data)
+
+        response = requests.post(url, json=data)
         del busy_info
         return response
+
+
+class AddForm(wx.Frame):
+    def __init__(self):
+        super(AddForm, self).__init__(None, title="Mój Formularz", size=(400, 300))
+
+        panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Dodaj tutaj elementy formularza
+
+        panel.SetSizer(sizer)
+        self.Layout()
+
+
+class RegisterPanel(wx.Panel):
+    def __init__(self, parent):
+        super(RegisterPanel, self).__init__(parent, size=(500, 800))
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.register_email_label = wx.StaticText(self, label="Podaj adres email")
+        sizer.Add(self.register_email_label, 0, wx.ALL | wx.CENTER, 5)
+        self.register_email_field = wx.TextCtrl(self)
+        sizer.Add(self.register_email_field, 0, wx.ALL | wx.CENTER, 5)
+        self.register_username_label = wx.StaticText(self, label="Podaj swój login")
+        sizer.Add(self.register_username_label, 0, wx.ALL | wx.CENTER, 5)
+        self.register_username_field = wx.TextCtrl(self)
+        sizer.Add(self.register_username_field, 0, wx.ALL | wx.CENTER, 5)
+        self.register_password_label = wx.StaticText(self, label="Wprowadź hasło")
+        sizer.Add(self.register_password_label, 0, wx.ALL | wx.CENTER, 5)
+        self.register_password_field = wx.TextCtrl(self, style=wx.TE_PASSWORD)
+        sizer.Add(self.register_password_field, 0, wx.ALL | wx.CENTER, 5)
+        self.register_password_confirm_label = wx.StaticText(self, label="Wprowadź ponownie hasło")
+        sizer.Add(self.register_password_confirm_label, 0, wx.ALL | wx.CENTER, 5)
+        self.register_password_confirm_field = wx.TextCtrl(self, style=wx.TE_PASSWORD)
+        sizer.Add(self.register_password_confirm_field, 0, wx.ALL | wx.CENTER, 5)
+        self.register_button = wx.Button(self, label="Zarejestruj się!")
+        self.register_button.Bind(wx.EVT_BUTTON, self.doRegister)
+        sizer.Add(self.register_button, 0, wx.ALL | wx.CENTER, 5)
+
+        self.SetSizer(sizer)
+
+    def doRegister(self, event):
+        username = self.register_username_field.GetValue()
+        email = self.register_email_field.GetValue()
+        password = self.register_password_field.GetValue()
+        confirm_password = self.register_password_confirm_field.GetValue()
+        data = {
+            'password': password,
+            'confirm_password': confirm_password,
+            'email': email,
+            'username': username
+        }
+        busy_info = wx.BusyInfo("Trwa rejestracja...")
+        url = "https://finanse.xce.pl/register"
+
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            data = response.json()
+            dane = data['dane']
+            print(dane)
+        elif response.status_code == 500:
+            print(response)
+        else:
+            print(f"Błąd: {response.status_code}")
+            data = response.json()
+            dane = data['error']
+            wx.MessageBox(dane, "Ostrzeżenie", wx.OK | wx.ICON_WARNING)
+        del busy_info
 
 
 class MainFrame(wx.Frame):
@@ -87,14 +161,17 @@ class MainFrame(wx.Frame):
         super(MainFrame, self).__init__(None, title="Finanses", size=(400, 300))
         self.mainPanel = MainPanel(self)
         self.listPanel = ListPanel(self)
+        self.registerPanel = RegisterPanel(self)
         self.listPanel.Hide()
-        self.SetSize(500, 500)
+        self.registerPanel.Hide()
+        self.SetSize(500, 400)
 
         self.currentPanel = self.mainPanel
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.mainPanel, 1, wx.EXPAND)
         self.sizer.Add(self.listPanel, 1, wx.EXPAND)
+        self.sizer.Add(self.registerPanel, 1, wx.EXPAND)
         self.SetSizer(self.sizer)
         menuBar = wx.MenuBar()
         quitMenu = wx.Menu()
@@ -102,8 +179,6 @@ class MainFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.on_exit, exit_item)
 
         menuBar.Append(quitMenu, '&Menu')
-
-        # Przypisanie paska menu do okna
         self.SetMenuBar(menuBar)
 
     def showListPanel(self):
@@ -112,6 +187,11 @@ class MainFrame(wx.Frame):
         self.listPanel.Show()
         self.Layout()
 
+    def showRegisterPanel(self):
+        self.currentPanel.Hide()
+        self.currentPanel = self.registerPanel
+        self.registerPanel.Show()
+        self.Layout()
 
     def on_exit(self, event):
         # Zamknięcie aplikacji
